b56c70cd3eec4f87113c769071d5a554b13d64a6
Jason Chitas <jasonchitas@hotmail.com>
2019-05-02 16:24:59 +0300

    v0.11 (ReadMe changes)

--- db/migrate/20190430135607_add_vin_and_plate_to_vehicles.rb
+++
@@ -1,6 +0,0 @@
-class AddVinAndPlateToVehicles < ActiveRecord::Migration[5.1]
-  def change
-    add_column :vehicles, :vehicle_identification_number, :string
-    add_column :vehicles, :vehicle_registration_plate, :string
-  end
-end
